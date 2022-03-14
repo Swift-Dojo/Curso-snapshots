2672e83e2207d3d43b453afe35a1c664dcd95d30
Mario Alberto Barragán Espinosa <mariosdev05@gmail.com>
2022-03-13 22:15:50 -0600

    Create empty state for our feed

--- CursoSnapshotsTests/CursoSnapshotsTests.swift
+++ CursoSnapshotsTests/CursoSnapshotsTests.swift
@@ -9,10 +9,33 @@ import XCTest
 @testable import CursoSnapshots
 import SnapshotTesting
 
+class FeedViewController: UIViewController {
+  
+  private lazy var emptyStateLabel: UILabel = {
+    let label = UILabel()
+    label.text = "Unable to get feed.\n Come again later"
+    label.textColor = UIColor(named: "fontColor")!
+    label.numberOfLines = 0
+    label.font = UIFont(name: "Helvetica Neue Bold", size: 24.0)
+    return label
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = UIColor(named: "background")!
+    view.addSubview(emptyStateLabel)
+    emptyStateLabel.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      emptyStateLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      emptyStateLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+    ])
+  }
+}
+
 class CursoSnapshotsTests: XCTestCase {
   
-  func test_emptyScreen() {
-    let sut = ViewController()
+  func test_emptyState() {
+    let sut = FeedViewController()
     
     assertSnapshot(matching: sut, as: .image(on: .iPhone8))
   }
